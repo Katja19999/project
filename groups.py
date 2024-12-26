3ec3712d671c064ab1238321c24f811111ec64ae
PogodinSergey <cameravolodarskogo@gmail.com>
2024-12-26 16:35:45 +0300

    Restarting Journey 1.1

---
+++ groups.py
@@ -0,0 +1,27 @@
+from pygame import sprite
+
+from constants import Constants
+
+
+# Group that manages "camera"
+class RelativeGroup(sprite.AbstractGroup):
+
+    absolute_position_h, absolute_position_v = Constants.absolute_center
+
+    def __init__(self):
+        super().__init__()
+
+        self.dh = 0
+        self.dv = 0
+
+    def update_camera(self, relative_position_h, relative_position_v):
+        self.dh = self.absolute_position_h - relative_position_h
+        self.dv = self.absolute_position_v - relative_position_v
+
+    def draw(self, surface, *args):
+        sprites = self.sprites()
+        surface_blit = surface.blit
+
+        for spr in sprites:
+            relative_pos_h, relative_pos_v = spr.rect.topleft
+            surface_blit(spr.image, (relative_pos_h - self.dh, relative_pos_v - self.dv))
